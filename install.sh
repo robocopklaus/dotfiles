03a281cb619e26d759bb3cb18ebb2a8f5757e55a
Fabian Pahl <fabian@21st.digital>
2020-12-26 18:55:46 +0100

    * Fixed CI steps * Refactored stuff

--- install.sh
+++ install.sh
@@ -1,10 +1,18 @@
 #!/usr/bin/env zsh
 
+isCommandAvailable() {
+  if command -v $1 >/dev/null; then
+    return
+  else
+    false
+  fi
+}
+
 ###############################################################################
 # Homebrew                                                                    #
 ###############################################################################
 
-if ! command -v brew >/dev/null; then
+if ! isCommandAvailable brew; then
   echo "Installing Homebrew..."
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 fi
