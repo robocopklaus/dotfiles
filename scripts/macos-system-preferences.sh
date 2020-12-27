e25123bdc9d6368f108b3e06f57bd9964b66ae2e
Fabian Pahl <fabian@21st.digital>
2020-12-27 21:47:24 +0100

    Added shebang

--- scripts/macos-system-preferences.sh
+++ scripts/macos-system-preferences.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 # Close any open System Preferences panes, to prevent them from overriding
 # settings we’re about to change
 osascript -e 'tell application "System Preferences" to quit'
