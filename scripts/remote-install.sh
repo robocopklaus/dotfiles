254ee678abf4ef02556169970b63d07adecd87fc
Fabian Pahl <fabian@21st.digital>
2020-12-28 00:23:19 +0100

    Fixed command line tools

--- scripts/remote-install.sh
+++ scripts/remote-install.sh
@@ -8,9 +8,22 @@ sudo -v
 sudo softwareupdate -i -a
 
 # Install Command Line Tools
-if [ $(xcode-select -p 1>/dev/null;echo $?) -eq 2 ]; then
-  xcode-select --install
+os=$(sw_vers -productVersion | awk -F. '{print $1 "." $2}')
+if softwareupdate --history | grep --silent "Command Line Tools.*${os}"; then
+    echo 'Command-line tools already installed.' 
+else
+    echo 'Installing Command-line tools...'
+    in_progress=/tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress
+    touch ${in_progress}
+    product=$(softwareupdate --list | awk "/\* Command Line.*${os}/ { sub(/^   \* /, \"\"); print }")
+    softwareupdate --verbose --install "${product}" || echo 'Installation failed.' 1>&2 && rm ${in_progress} && exit 1
+    rm ${in_progress}
+    echo 'Installation succeeded.'
 fi
+
+# if [ $(xcode-select -p 1>/dev/null;echo $?) -eq 2 ]; then
+#   xcode-select --install
+# fi
 
 if [ ! -d "$DOTFILES_PATH" ]; then
   echo "Installing dotfiles..."
