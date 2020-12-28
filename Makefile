0984d5b1960e2d08fdd87903c492545c15b36cbb
Fabian Pahl <fabian@21st.digital>
2020-12-28 13:52:43 +0100

    Fixes

--- Makefile
+++ Makefile
@@ -22,6 +22,8 @@ brew-packages: brew
 
 oh-my-zsh:
 	@is-directory $(HOME)/.oh-my-zsh || curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh | bash
+	@rm -f ~/.zcompdump
+  @compaudit | xargs chmod g-w,o-w
 
 package-post-install-fixes:
 	@$(SHELL) scripts/post-install-iterm2-fix.sh
--- scripts/remote-install.sh
+++ scripts/remote-install.sh
@@ -8,10 +8,12 @@ sudo -v
 sudo softwareupdate -i -a
 
 # Command Line Tools
-touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress;
-PROD=$(softwareupdate -l | grep "*.*Command Line" | tail -n 1 | awk -F"*" '{print $2}' | sed -e 's/^ *//' | sed 's/Label: //g' | tr -d '\n')
-softwareupdate -i "$PROD" --verbose
-rm /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress
+if ! [ -e "/Library/Developer/CommandLineTools/usr/bin/git" ]; then
+  touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress;
+  PROD=$(softwareupdate -l | grep "*.*Command Line" | tail -n 1 | awk -F"*" '{print $2}' | sed -e 's/^ *//' | sed 's/Label: //g' | tr -d '\n')
+  softwareupdate -i "$PROD" --verbose
+  rm /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress
+fi
 
 if [ ! -d "$DOTFILES_PATH" ]; then
   echo "Installing dotfiles..."
